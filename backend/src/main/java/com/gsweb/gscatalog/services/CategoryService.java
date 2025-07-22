06e60ae3a123ce1617e07fd65ef32f57356089a7
Gabriel_Sousa <gabriel.sist.inf@gmail.com>
2025-07-22 09:44:03 -0300

    DTO

--- backend/src/main/java/com/gsweb/gscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gsweb/gscatalog/services/CategoryService.java
@@ -1,21 +1,24 @@
 package com.gsweb.gscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gsweb.gscatalog.dto.CategoryDTO;
 import com.gsweb.gscatalog.entities.Category;
 import com.gsweb.gscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll() {
-		return repository.findAll();
+
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDto;
 	}
-	
 }

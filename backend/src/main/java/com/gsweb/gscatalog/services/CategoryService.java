8d1aecbffe40aa3bc9e5403c93750bd5a286931e
Gabriel_Sousa <gabriel.sist.inf@gmail.com>
2025-07-23 08:18:35 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/gsweb/gscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gsweb/gscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.gsweb.gscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gsweb.gscatalog.dto.CategoryDTO;
 import com.gsweb.gscatalog.entities.Category;
@@ -16,9 +18,17 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		return listDto;
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new com.gsweb.gscatalog.services.exceptions.EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
